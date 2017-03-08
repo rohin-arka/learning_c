05daa30716661e9ed6fa3543b4a203513d4f1e8b
Surya <surya@getrhombus.com>
2017-03-08 23:20:45 +0545

    completed exercises3 printing without next line

---
+++ exercises/exercise3.c
@@ -0,0 +1,18 @@
+// includes pre process header file stdio.h
+#include <stdio.h>
+// function that executes when program runs
+int main(void){
+  // printing stings
+  printf("Testing...");
+  printf("....1");
+  printf("...2");
+  printf("..3");
+  // printing next line
+  printf("\n");
+  // terminating program with return value 0 which is standard convention
+  return 0;
+}
+
+// Points to learn
+  // Here without \means print in the same line
+  // eg: the output of following program is: Testing.......1...2..3
