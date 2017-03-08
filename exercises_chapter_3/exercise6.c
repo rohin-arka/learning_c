a4f35f3273ca00ba61adbe9ca955e50049a6c762
Surya <surya@getrhombus.com>
2017-03-08 23:55:37 +0545

    completed problem set of exercise 6 guessing expecting output

---
+++ exercises_chapter_3/exercise6.c
@@ -0,0 +1,12 @@
+// includes prepocessor header file stdio.h
+#include <stdio.h>
+// function executes on execution of progrsm
+int main(void){
+  // declating int type variable
+  int answer, result;
+  answer = 100;
+  result = answer - 10;
+  // prints subtracting 5 from result i.e. 95
+  printf("The exepected outputs is %i\n", result + 5);
+  return 0;
+}
