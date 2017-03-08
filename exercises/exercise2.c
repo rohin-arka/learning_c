33f2228f4f1e49ec834f0bfd3b80d6d32da6bed7
Surya <surya@getrhombus.com>
2017-03-08 23:13:29 +0545

    completed chapter 3 exercise 2 from the book

---
+++ exercises/exercise2.c
@@ -0,0 +1,15 @@
+// includes header file to the current program
+#include <stdio.h>
+
+// function which runs in program execution
+// void is argument in function
+int main(void){
+
+  // prints all the string to standard output
+  // \ n is next line and ; is to end function printf
+  printf("In C lower case letters are significant\n");
+  printf("Main is function where program execustion begins\n");
+  printf("All program statements must be terminated by a semicolon\n");
+  // exits the programs by returning 0
+  return 0;
+}
