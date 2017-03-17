48f2f0915d55987f437ac384985538cfc7af2778
rohin-arka <surya_siwakoti@outlook.com>
2017-03-17 23:32:09 +0545

    completed problem set for first number divisible by second

---
+++ exercises_chapter_6/exercise1.c
@@ -0,0 +1,18 @@
+// Write a program that asks the user to type in two integer values at the terminal.
+// Test these two numbers to determine if the first is evenly divisible by the second,
+// and then display an appropriate message at the terminal.
+#include <stdio.h>
+int main(void){
+  int firstNumber, secondNumber;
+
+  printf("Enter the first number\n");
+  scanf("%i", &firstNumber);
+
+  printf("Enter the second number\n");
+  scanf("%i", &secondNumber);
+
+  if ((firstNumber % secondNumber) == 0)
+    printf("first number is divisible by second\n");
+  else
+    printf("first number is not divisible by second number\n");
+}
