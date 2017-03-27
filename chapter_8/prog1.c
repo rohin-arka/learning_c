ddabc614429fc9bcf96a0f75dcbb63eae959ec3e
rohin-arka <surya_siwakoti@outlook.com>
2017-03-27 20:21:04 +0545

    complete function first program

---
+++ chapter_8/prog1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void printMessage(){
+  printf("Programming is fun from print Message function\n");
+}
+int main(void){
+  printf("Programming is fun from main function\n");
+  printMessage();
+  return 0;
+}
